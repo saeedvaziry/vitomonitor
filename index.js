473eaddd74ec1a6d67bf540e44e0de497d5bbedf
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2021-12-14 01:08:54 +0400

    first init

---
+++ index.js
@@ -0,0 +1,44 @@
+const schedule = require('node-schedule');
+const axios = require('axios').default;
+const os = require('node-os-utils');
+const fs = require('fs');
+
+fs.readFile('/usr/local/share/vmonitor/config.json', 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const config = JSON.parse(data);
+    schedule.scheduleJob('* * * * *', async function() {
+      let cpu = await getCPU();
+      let mem = await getMemory();
+      let disk = await getDisk();
+      send(config.url, cpu, mem, disk);
+    });
+  }
+});
+
+function send(url, cpu, memory, disk) {
+  axios.post(url, {
+    cpu: cpu,
+    memory: memory,
+    disk: disk
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+async function getCPU() {
+  let cpu = await os.cpu.usage();
+  return Number(cpu).toFixed(2);
+}
+
+async function getMemory() {
+  let mem = await os.mem.used();
+  let used = (mem.usedMemMb / mem.totalMemMb) * 100;
+  return Number(used).toFixed(2);
+}
+
+async function getDisk() {
+  let disk = await os.drive.used("/");
+  return Number(disk.usedPercentage).toFixed(2);
+}
