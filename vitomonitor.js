8f441dcf486b4fc099c8aabe666fae42a5ab6527
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2021-12-29 14:59:40 +0400

    add service status and server updates

--- vitomonitor.js
+++ vitomonitor.js
@@ -4,12 +4,17 @@ const schedule = require('node-schedule');
 const axios = require('axios').default;
 const os = require('node-os-utils');
 const fs = require('fs');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
-function send(url, cpu, memory, disk) {
+function send(url, cpu, memory, disk, updates, securityUpdates, services) {
   axios.post(url, {
     cpu: cpu,
     memory: memory,
-    disk: disk
+    disk: disk,
+    updates: updates,
+    security_updates: securityUpdates,
+    services: services
   }).catch((err) => {
     console.log(err);
   });
@@ -31,6 +36,29 @@ async function getDisk() {
   return Number(disk.usedPercentage).toFixed(2);
 }
 
+async function getUpdates() {
+  let updates = await exec('apt list --upgradable 2>/dev/null | wc -l');
+  return Number(updates.stdout);
+}
+
+async function getSecurityUpdates() {
+  let updates = await exec('apt list --upgradable 2>/dev/null | grep "\-security" | wc -l');
+  return Number(updates.stdout);
+}
+
+async function getServiceStatus(services) {
+  let statuses = {};
+  for (let i = 0; i < services.length; i++) {
+    let status = await exec(`systemctl status ${services[i]}`);
+    if (status.stdout.toString().includes('active (running)')) {
+      statuses[services[i]] = true;
+    } else {
+      statuses[services[i]] = false;
+    }
+  }
+  return statuses;
+}
+
 function main() {
   fs.readFile('/usr/local/share/vitomonitor/config.json', 'utf8', (err, data) => {
     if (err) {
@@ -41,7 +69,21 @@ function main() {
         let cpu = await getCPU();
         let mem = await getMemory();
         let disk = await getDisk();
-        send(config.url, cpu, mem, disk);
+        let updates = await getUpdates();
+        let securityUpdates = await getSecurityUpdates();
+        if (!config.services) {
+          config.services = [];
+        }
+        let statuses = await getServiceStatus(config.services);
+        send(
+          config.url,
+          cpu,
+          mem,
+          disk,
+          updates,
+          securityUpdates,
+          statuses
+        );
       });
     }
   });
